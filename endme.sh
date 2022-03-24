fef4e411f2821f3f0847e017cda33501cc297d5f
joseph-redmond <josephredmond9121@gmail.com>
2022-03-23 19:16:21 -0700

    Modified run and end scripts

--- endme.sh
+++ endme.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 docker kill demoauthapi
+docker rm demoauthapi
 docker kill maildev
+docker rm maildev
 docker network rm authapi-net
 
 echo "All systems down. Traces Terminated :)"
--- startme.sh
+++ startme.sh
@@ -3,8 +3,8 @@
 ./gradlew clean build
 docker image build -t demoauthapi .
 docker network create authapi-net
-
 docker run -d -p 8080:8080 --net authapi-net --name demoauthapi demoauthapi
+echo "You're good to go :)"
+echo "Run endme.sh when you're done to clean docker up"
 docker run --net authapi-net -p 1080:1080 -p 1025:1025 --name maildev soulteary/maildev
 
-echo "You're good to go :)"
